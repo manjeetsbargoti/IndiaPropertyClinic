f57baa0cbda6ae3de154187d6e300260df77076c
pankajirvine <pankajkumargupta@isupportcomputer.org>
2019-04-09 18:36:59 +0530

    Change admin footer text

--- resources/views/layouts/adminLayout/admin_footer.blade.php
+++ resources/views/layouts/adminLayout/admin_footer.blade.php
@@ -2,7 +2,7 @@
   <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-      
+      Admin Panel
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2019 <a href="#">India Property Clinic</a>.</strong> All rights reserved.
